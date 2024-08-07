f2f0edcb259d76a0e1180e1ab4032d17066b537a
TIJANI0826 <ibrahimtijani08@gmail.com>
2024-08-07 18:12:20 +0100

    PyTechMentors first tutorial

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+print("Hello world")
+# input("type your name: ")
+
+#Data Types
+# Number : interger, float, complex, Bool =True or False
+# Strings ""
+# List
+# Tuples
+# Dictionaries
+# Set
+
+#personal information
+country = 'Nigeria'
+population = 2000000
+height = 3.5
+student = True
+latitude = 10 + 20j
+# name = input("Type your name: ")
+# age = int(input("Type in your age: "))
+
+# print(name)
+# print(age)
+print(type(population))
+print(type(height))
+print(type(latitude))
