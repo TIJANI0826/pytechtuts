beb3b6aa46fff932177f962db485329716e74d1d
TIJANI0826 <ibrahimtijani08@gmail.com>
2024-08-09 18:19:25 +0100

    Added agecalculator.py

---
+++ agecalculator.py
@@ -0,0 +1,5 @@
+print("This program calculates the age of the person given his year of birth")
+year = int(input("Type your year of birth: "))
+this_year = 2024
+age = this_year - year
+print("Your age is " + str(age))
--- tax.py
+++ tax.py
@@ -1,1 +1,10 @@
 # write a program that will calculate simple interest I = PRT/100
+print("This program calculates simple interest")
+principal = float(input("Type the principal: "))
+rate = float(input("Type the rate e.g 5 for 5%, 20 for 20% etc.: "))
+time = int(input("Type the numbers year for the loan: "))
+
+simple_interest = (principal * rate * time) / 100
+print("The simple interest is " + str(simple_interest))
+paying = simple_interest + principal
+print("This means you are paying " + str(paying) + " after " + str(time) + " years." )
